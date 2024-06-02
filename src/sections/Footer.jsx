9dda6f356bb5c696c46144633df08de1d71ce31d
Shanmukha <55737606+pshanmukha@users.noreply.github.com>
2024-06-03 02:32:23 +0530

    base path updated

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="max-container">
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
-          <a href="/">
+          <a href="/nike/">
             <img
               src={footerLogo}
               alt="logo"
